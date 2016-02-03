055e9b36b22f04db48dd02a529506fc18cef50c4
Kairat Doshekenov <Kairat Doshekenov>
2016-02-03 11:39:46 +0600

    Initial commit

---
+++ src/main/java/com/epam/matricemult/MainClass.java
@@ -0,0 +1,11 @@
+package com.epam.matricemult;
+
+/**
+ * Created by Kairat Dos on 02/02/2016.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Matrice multiplier started");
+
+    }
+}
---
+++ src/main/java/com/epam/matricemult/Multiplier.java
@@ -0,0 +1,7 @@
+package com.epam.matricemult;
+
+/**
+ * Created by Kairat Dos on 02/02/2016.
+ */
+public class Multiplier {
+}
